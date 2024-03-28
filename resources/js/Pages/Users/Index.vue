c74cbfd5d073cb73faecb0ac1ca1e0236bcd782f
Jahanzaib khan <jahanzaibkhan9999@gmail.com>
2024-03-29 02:31:17 +0500

    Initial commit

---
+++ resources/js/Pages/Users/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>User List</h1>
+    <ul>
+      <li v-for="user in users.data" :key="user.id">{{ user.name }}</li>
+    </ul>
+    <inertia-link :href="users.prev_page_url">Previous</inertia-link>
+    <inertia-link :href="users.next_page_url">Next</inertia-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    users: Object, // Passed from Laravel controller
+  },
+};
+</script>
